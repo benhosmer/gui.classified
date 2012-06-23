63a611ab3d03dbbfe6ee725cc1e6ae9f323ef837
Ben Hosmer <ben@radarearth.com>
2012-06-22 20:27:44 -0400

    class.PluginClassified.php is for module logic

---
+++ class.PluginClassified.php
@@ -0,0 +1,42 @@
+<?php
+
+defined('AJXP_EXEC') or die( 'Access not allowed');
+
+/**
+ * @package info.ajaxplorer.plugins
+ * A plugin to display the site classification level.
+ */
+class PluginClassified extends AJXP_Plugin {
+
+    /**
+     * @param DOMNode $contribNode
+     * @return void
+     */
+    public function parseSpecificContributions(&$contribNode){
+        if($contribNode->nodeName != "client_configs") return;
+        $actionXpath=new DOMXPath($contribNode->ownerDocument);
+        $footerTplNodeList = $actionXpath->query('template[@name="bottom"]', $contribNode);
+        $footerTplNode = $footerTplNodeList->item(0);
+
+        
+        
+        //$content = $this->pluginConf["CUSTOM_FOOTER_CONTENT"];
+
+        $content = $this->pluginConf["SITE_CLASSIFICATION_LEVEL"];
+        
+
+        $content = str_replace("\\n", "<br>", $content);
+        //$cdata = '<div id="optional_bottom_div" style="font-family:arial;padding:10px;">'.$content.'</div>';
+        //$cdata = '<div id="optional_header_div" style="font-family:arial;padding:10px;">'.$content.'</div>';
+        
+        $cdata = '<div id="optional_bottom_div">Bottom Div here</div';
+
+
+        $cdataSection = $contribNode->ownerDocument->createCDATASection($cdata);
+        //foreach($footerTplNode->childNodes as $child) $footerTplNode->removeChild($child);
+        $footerTplNode->appendChild($cdataSection);
+        
+    }
+}
+
+?>
