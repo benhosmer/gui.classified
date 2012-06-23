d3e9e76bd2db39ce141aa5ec91742cd991dd8126
Ben Hosmer <ben@radarearth.com>
2012-06-23 07:04:49 -0400

    Refactor -- basically using skeleton as a model.

--- class.PluginClassified.php
+++ class.PluginClassified.php
@@ -4,7 +4,7 @@ defined('AJXP_EXEC') or die( 'Access not allowed');
 
 /**
  * @package info.ajaxplorer.plugins
- * A plugin to display the site classification level.
+ * A plugin to replace the header and footer with the site classification level.
  */
 class PluginClassified extends AJXP_Plugin {
 
@@ -17,25 +17,12 @@ class PluginClassified extends AJXP_Plugin {
         $actionXpath=new DOMXPath($contribNode->ownerDocument);
         $footerTplNodeList = $actionXpath->query('template[@name="bottom"]', $contribNode);
         $footerTplNode = $footerTplNodeList->item(0);
-
-        
-        
-        //$content = $this->pluginConf["CUSTOM_FOOTER_CONTENT"];
-
         $content = $this->pluginConf["SITE_CLASSIFICATION_LEVEL"];
-        
-
         $content = str_replace("\\n", "<br>", $content);
-        //$cdata = '<div id="optional_bottom_div" style="font-family:arial;padding:10px;">'.$content.'</div>';
-        //$cdata = '<div id="optional_header_div" style="font-family:arial;padding:10px;">'.$content.'</div>';
-        
-        $cdata = '<div id="optional_bottom_div">Bottom Div here</div';
-
-
+        $cdata = '<div id="optional_bottom_div" style="font-family:arial;padding:10px;">'.$content.'</div>';
         $cdataSection = $contribNode->ownerDocument->createCDATASection($cdata);
-        //foreach($footerTplNode->childNodes as $child) $footerTplNode->removeChild($child);
-        $footerTplNode->appendChild($cdataSection);
-        
+        foreach($footerTplNode->childNodes as $child) $footerTplNode->removeChild($child);
+        $footerTplNode->appendChild($cdataSection);   
     }
 }
 
